1f3b9d05e448239b31d24c146c8cb88583dedaaa
Leo <leonardo.soares99@edu.pucrs.br>
2022-05-28 14:14:21 -0300

    feat/US04FE - Leonardo e Gustavo

---
+++ Desenvolvimento/Frontend/reforco_app/lib/view/home/home_controller.dart
@@ -0,0 +1,14 @@
+// import 'package:comunicacao_hsl/model/network/login.dart';
+
+// class LoginController {
+
+//     Future<void> login(String registration, Function onCompletion) async {
+//         var parsedRegistration = int.tryParse(registration);
+        
+//         if (parsedRegistration != null) {
+//           return onCompletion(await LoginModel().connect(parsedRegistration));
+//         } 
+//         return onCompletion(false);
+//     }
+
+// }
